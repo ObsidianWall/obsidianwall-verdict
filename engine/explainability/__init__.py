e1ee60749a2ecd0396d94a731ba9b5021fba8bc7
Aisha <121854477+Kxanx1538@users.noreply.github.com>
2026-05-17 00:57:19 -0400

    Add __init__.py for explainability module imports

---
+++ engine/explainability/__init__.py
@@ -0,0 +1,18 @@
+
+# engine/explainability/__init__.py
+
+from engine.explainability.explanation_builder import (
+    build_explanation_artifact
+)
+
+from engine.explainability.policy_reasoning import (
+    build_policy_reasoning
+)
+
+from engine.explainability.recommendation_explainer import (
+    explain_recommendations
+)
+
+from engine.explainability.trace_graph import (
+    build_trace_graph
+)
